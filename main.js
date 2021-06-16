8264c792cde3df300b2d321d25b3eaf65df47736
Aleks <aleksandar.trpkovski@gmail.com>
2021-06-16 22:41:19 +1000

    added listener to the form

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
 import "./bootstrap.css";
 import "./style.scss";
+
+// Add listener to the submit button
+const form = document.getElementById("form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const allFields = event.target;
+  const firstName = allFields[0].value;
+  const lastName = allFields[1].value;
+  const email = allFields[2].value;
+  const mobileNumber = allFields[3].value;
+  const subject = allFields[4].value;
+  const message = allFields[5].value;
+
+  console.log(firstName);
+  console.log(lastName);
+  console.log(email);
+  console.log(mobileNumber);
+  console.log(subject);
+  console.log(message);
+});
